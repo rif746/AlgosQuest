5283e1fc6aab3b1e85a9330325b65c8b1c38f89a
Syarif Ubaidillah <ubed56pb@gmail.com>
2023-09-13 22:28:51 +0700

    fix: back to previous scene.

---
+++ Readme.md
@@ -0,0 +1,14 @@
+## The Lost Key
+
+repositori untuk projek skripsi **Universitas Bahaudin Mudhary Madura** Program Studi **Informatika**.
+
+## Introduction
+
+Game ini bertujuan untuk memberikan pembelajaran dasar **Algoritma dan Struktur Data**. Genre dari game ini adalah _Maze Escape_, dimana pemain harus mencari beberapa objek dan menjawab pertanyaan seputar materi tertentu untuk menyelesaikan permainan. setiap _stage_ berisi 1 materi pembelajaran, dengan beberapa pertanyaan yang terkait sebagai _quest_ dalam game. materi dapat diperoleh setelah _stage_ berhasil diselesaikan.
+
+## Development
+
+- Game Engine: [Godot Engine](https://github.com/godotengine/godot)
+- Platform: Android
+- Target Android SDK: 33
+- Minimum Android SDK: 24
--- globals/scene/scene_changer.gd
+++ globals/scene/scene_changer.gd
@@ -1,7 +1,10 @@
 extends CanvasLayer
 
+var path_history: Array[String]
+
 func change_scene(scene):
 	$AnimationPlayer.play("dissolve")
+	make_history_scene(scene)
 	await $AnimationPlayer.animation_finished
 	
 	if scene is PackedScene:
@@ -12,8 +15,27 @@ func change_scene(scene):
 	$AnimationPlayer.play_backwards("dissolve")
 
 
+func make_history_scene(scene):
+	var current_scene_file_path = get_tree().current_scene.scene_file_path
+	if (path_history.has(scene)):
+		path_history.pop_back()
+	elif current_scene_file_path != "res://gui/game/hud.tscn":
+		path_history.append(current_scene_file_path)
+
+
+func back_to_previous_scene():
+	# if in main menu
+	if path_history.size() == 0:
+		return true
+	
+	var history_size = path_history.size()
+	var scene_file = path_history[history_size - 1]
+	change_scene(scene_file)
+
+
 func change_scene_with_callback(scene, callback: Callable):
 	$AnimationPlayer.play("dissolve")
+	make_history_scene(scene)
 	await $AnimationPlayer.animation_finished
 	
 	if scene is PackedScene:
